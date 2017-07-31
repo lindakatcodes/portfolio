b8188c137d3661f38b271be675f0bbe9c44ee96f
Kat <lindakt16@gmail.com>
2017-07-30 21:36:20 -0500

    Initial set up, remove past versions for fresh start

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 const gulp = require('gulp'),
 watch = require('gulp-watch'),
-browserSync = require('browser-sync').create();
+browserSync = require('browser-sync').create(),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssVars = require('postcss-simple-vars');
+
+gulp.task('styles', function() {
+    return gulp.src('./styleguide.css')
+    .pipe(postcss([cssVars, autoprefixer]))
+    .on('error', function(errorInfo) {
+        console.log(errorInfo.toString());
+        this.emit('end');
+    })
+    .pipe(gulp.dest('./styles'));
+})
 
 gulp.task('watch', function() {
     browserSync.init({
@@ -15,6 +28,11 @@ gulp.task('watch', function() {
     });
 
     watch('./styleguide.css', function() {
-        browserSync.stream();
+        gulp.start('cssStyles');
     });
+});
+
+gulp.task('cssStyles', ['styles'], function() {
+    return gulp.src('./styles/styleguide.css')
+    .pipe(browserSync.stream());
 })
