e3c7135b9aa504aaf1daa9f881d09df02d886c5a
Lindakat <lindakt16@gmail.com>
2019-01-30 20:20:13 -0600

    Got projects showing on page; minor changes to semantics of html page

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => results);
 
 const dataURL = `http://localhost:8080/assets/data/datafile.json`;
 
-const dataDiv = document.querySelector('.datadiv');
+const projectDiv = document.querySelector('.project-div');
 
 const results = fetch(dataURL)
   .then(res => res.json())
@@ -11,8 +11,33 @@ const results = fetch(dataURL)
     const projects = res.projects;
     projects.forEach(project => {
       const pblock = document.createElement('div');
+      pblock.classList.add('project-block');
       
-      const pname = document.createElement('h3');
+      /* For images: */
+      // First, grab the image data from datafile & store it
+      const pImages = project.image;       
+      // Next, make a var for the image block as a whole
+      const pimgs = document.createElement('div');
+      // Then, if there's only one image, create a single tag and set the src, then add to image block
+      if (pImages.length === 1) {
+        const pimg = document.createElement('img');
+        pimg.src = pImages[0];
+        pimg.alt = project.altText;
+        pimgs.append(pimg);
+      } else if (pImages.length > 1) {
+        // If there's multiple, create a tag for each one - add each to the image block
+        pImages.forEach(image => {
+          const thisImg = document.createElement('img');
+          thisImg.src = image;
+          thisImg.alt = project.altText;
+          pimgs.append(thisImg);
+        });
+      }
+      
+      // Finally, add the image block to the pblock
+      pblock.append(pimgs);
+
+      const pname = document.createElement('h4');
       pname.innerHTML = project.name;
       pblock.append(pname);
 
@@ -20,16 +45,22 @@ const results = fetch(dataURL)
       pdesc.innerHTML = project.description;
       pblock.append(pdesc);
 
-      const pimg = document.createElement('img');
-      pimg.src = project.image;
-      pimg.alt = project.altText;
-      pblock.append(pimg);
+      const ptech = document.createElement('p');
+      ptech.innerHTML = project.tech;
+      pblock.append(ptech);
 
-      const plink = document.createElement('a');
-      plink.href = project.link;
-      plink.innerHTML = 'View Project';
-      pblock.append(plink);
+      if (project.siteLink !== "") {
+        const psitelink = document.createElement('a');
+        psitelink.href = project.siteLink;
+        psitelink.innerHTML = 'View Project';
+        pblock.append(psitelink);
+      }
 
-      dataDiv.append(pblock);
+      const pcodelink = document.createElement('a');
+      pcodelink.href = project.codeLink;
+      pcodelink.innerHTML = 'View the Code';
+      pblock.append(pcodelink);
+
+      projectDiv.append(pblock);
     })
   });
