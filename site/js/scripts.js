8202f2b5090d6719ecbcce34d7b4846f59b20bd0
Lindakat <lindakt16@gmail.com>
2019-02-08 22:42:06 -0600

    Added certificate, about, and contact info to site - struggling with styling of projects responsively

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -4,16 +4,24 @@ document.addEventListener('DOMContentLoaded', () => results);
 // address for projects/certificates
 const dataURL = `http://localhost:8080/assets/data/datafile.json`;
 
-/* Build Project HTMl & Add to Site */
-const projectDiv = document.querySelector('.project-div');
+// Save the project & certificate sections from the HTML
+const projectDiv = document.querySelector('.project-section');
+const certDiv = document.querySelector('.cert-section');
 
+/* Grab data from the datafile and display on site */
 const results = fetch(dataURL)
   .then(res => res.json())
   .then(res => {
+    // First - projects
     const projects = res.projects;
     projects.forEach(project => {
       const pblock = document.createElement('div');
       pblock.className = 'project-block';
+
+      const pname = document.createElement('h4');
+      pname.innerHTML = project.name;
+      pname.className = 'project-name';
+      pblock.append(pname);
       
       /* For images: */
       // First, grab the image data from datafile & store it
@@ -25,40 +33,18 @@ const results = fetch(dataURL)
       // Then, build the img tag, with srcset and alt text included
       pImages.forEach(image => {
         const thisImg = document.createElement('img');
-        console.log(image);
         thisImg.srcset = `
         ${image[0]} 420w,
         ${image[1]} 800w,
         ${image[2]}`;
         thisImg.src = image[2];
         thisImg.alt = project.altText;
+        thisImg.sizes = `(max-width: 420px) 90vw, (max-width: 800px) 50vw, 40vw`;
         pimgs.append(thisImg);
       })
       
-      
-      // Then, if there's only one image, create a single tag and set the src, then add to image block
-      /* if (pImages.length === 1) {
-        const pimg = document.createElement('img');
-        pimg.src = pImages[0];
-        pimg.alt = project.altText;
-        pimgs.append(pimg);
-      } else if (pImages.length > 1) {
-        // If there's multiple, create a tag for each one - add each to the image block
-        pImages.forEach(image => {
-          const thisImg = document.createElement('img');
-          thisImg.src = image;
-          thisImg.alt = project.altText;
-          pimgs.append(thisImg);
-        }); 
-      } */
-      
       // Finally, add the image block to the pblock
       pblock.append(pimgs);
-
-      const pname = document.createElement('h4');
-      pname.innerHTML = project.name;
-      pname.className = 'project-name';
-      pblock.append(pname);
 
       const pdesc = document.createElement('p');
       pdesc.innerHTML = project.description;
@@ -95,4 +81,28 @@ const results = fetch(dataURL)
 
       projectDiv.append(pblock);
     })
+
+    // Then - certificates
+    const certs = res.certificates;
+    certs.forEach(cert => {
+      const cFig = document.createElement('figure');
+      cFig.className = 'cert-block';
+
+      const cImg = document.createElement('img');
+      cImg.srcset = `
+      ${cert.cert[0]} 420w,
+      ${cert.cert[1]} 800w,
+      ${cert.cert[2]}`;
+      cImg.src = cert.cert[2];
+      cImg.alt = `Certificate earned for completion of ${cert.class} course`;
+      cImg.sizes = `(max-width: 420px) 90vw, (max-width: 800px) 50vw, 40vw`;
+      cFig.append(cImg);
+      
+      const cCaption = document.createElement('figcaption');
+      cCaption.innerText = cert.class;
+
+      cFig.append(cCaption);
+
+      certDiv.append(cFig);
+      })
   });
