0318880f6561b4440ece93f4dfe2a9ee0b08bfd1
Lindakat <lindakt16@gmail.com>
2019-02-12 22:54:37 -0600

    Renamed index file

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ function prepJS() {
 
 // grab HTML files, minify, then send to docs
 function prepHTML() {
-    return src('./site/index-new.html', { sourcemaps: true })
+    return src('./site/index.html', { sourcemaps: true })
     .pipe(htmlmin())
     .pipe(dest('./docs', { sourcemaps: '.' }));
 }
@@ -100,7 +100,7 @@ function watcher() {
     browserSync.init({
         server: {
             baseDir: './docs',
-            index: 'index-new.html'
+            index: 'index.html'
         },
         port: 8080
     });
